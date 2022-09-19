38209eddd1cfe0ed0896da260248ffb4593ad583
digvijay mudde <90601525+digvijaymudde@users.noreply.github.com>
2022-09-19 14:07:38 +0530

    Create Addition.py

---
+++ Addition.py
@@ -0,0 +1,15 @@
+# PythonPractice
+python simple problem solving 
+
+
+
+
+print ("Enter the first Number :")
+x=int(input())
+
+print("enter the second number :")
+y=int(input())
+
+z=x+y
+
+print(z)
